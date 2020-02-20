ead05f682d74d535b5e3cdc3a580755b283fe138
cahyaekapermana <cahyaekapermana@gmail.com>
2020-02-21 00:27:07 +0700

    Pembuatan form android bundle Progress : - Set Form - Get Form - Required Form
    Miss :
    - Gallery
    - Password Check with Confirm password

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -3,12 +3,45 @@ package id.ac.polinema.intentexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView fullnameText;
+    private TextView emailText;
+    private TextView homepageText;
+    private TextView aboutText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+//        Bind dari view activity main
+        fullnameText = findViewById(R.id.label_fullname);
+        emailText = findViewById(R.id.label_email);
+        homepageText = findViewById(R.id.label_homepage);
+        aboutText = findViewById(R.id.label_about);
+
+        Bundle extras = getIntent().getExtras();
+
+        String fullnameValue = extras.getString("fullname");
+        String emailValue = extras.getString("email");
+        String homepageValue = extras.getString("homepage");
+        String aboutValue = extras.getString("about");
+
+        //  Get Text dari Bind
+        if (extras != null) {
+
+            // TODO: display value here
+            fullnameText.setText(fullnameValue);
+            emailText.setText(emailValue);
+            homepageText.setText(homepageValue);
+            aboutText.setText(aboutValue);
+
+        }
+
     }
+
 }
